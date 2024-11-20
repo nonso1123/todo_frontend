52ae1fce839e7655b5bbeb1fc7723f75fae1e63d
nonso1123 <chinonsonwora@gmail.com>
2024-11-20 16:29:29 +0100

    added backend url

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://todo-app-bq06.onrender.com/api/",
+  baseURL: "https://todo-app-bq06.onrender.com/api",
 });
 
 export default api;
